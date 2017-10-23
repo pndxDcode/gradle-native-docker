a54c907d1e187700bd8a11ced6225e05ad929c22
pranasabda <pranasabda@gmail.com>
2017-10-23 16:16:42 +0700

    Merge File and Add some code for Intent to Gitlogin.java

--- app/src/main/java/id/refactory/app/refactoryapps/MainActivity.java
+++ app/src/main/java/id/refactory/app/refactoryapps/MainActivity.java
@@ -1,7 +1,7 @@
 package id.refactory.app.refactoryapps;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -23,59 +23,28 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener,
-        WPMFragment.OnFragmentInteractionListener, HRFragment.OnFragmentInteractionListener, SOFFragment.OnFragmentInteractionListener,
-        OSFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener,WPMFragment.OnFragmentInteractionListener, HRFragment.OnFragmentInteractionListener, SOFFragment.OnFragmentInteractionListener,OSFragment.OnFragmentInteractionListener
+    {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setContentView(R.layout.test_dataview);
+        //  //Calling method dashBoard that has been made below
+       // dashBoard();
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
-
-        // set tab head tobe title
-//        tabLayout.addTab(tabLayout.newTab().setText("Project"));
-//        tabLayout.addTab(tabLayout.newTab().setText("WPM"));
-//        tabLayout.addTab(tabLayout.newTab().setText("HackerRank"));
-//        tabLayout.addTab(tabLayout.newTab().setText("StackOverFlow"));
-//        tabLayout.addTab(tabLayout.newTab().setText("OpenSource"));
-
-         //set tab head tobe Icon
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_projects));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_wpm));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_hr));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_sof));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_os));
-
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-
-
-
+        gitLogin();
     }
+
+// //Call Dashboard.class and run it
+//    public void dashBoard() {
+//        Intent intents = new Intent(this, Dashboard.class);
+//        startActivity(intents);
+//    }
+
+        public void gitLogin(){
+            Intent intent = new Intent(this, GitLogin.class);
+            startActivity(intent);
+        }
+
 }
