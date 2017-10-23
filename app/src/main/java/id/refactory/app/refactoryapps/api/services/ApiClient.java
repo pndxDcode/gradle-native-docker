dea68185c91b8d0e4bf4d27b540f942adc032628
pranasabda <pranasabda@gmail.com>
2017-10-23 16:25:17 +0700

    Merge File ApiClient.java From Sam

--- app/src/main/java/id/refactory/app/refactoryapps/api/services/ApiClient.java
+++ app/src/main/java/id/refactory/app/refactoryapps/api/services/ApiClient.java
@@ -5,4 +5,44 @@ package id.refactory.app.refactoryapps.api.services;
  */
 
 public class ApiClient {
+    private String code;
+    private String URL = "https://api.refactory.id/";
+    private String grant_type = "authorization_code";
+    private String client_id = "4";
+    private String client_secret = "3fd37387ea110499059222a6ed1fa235";
+    private final String redirect_uri = "https://app.refactory.id/callback";
+    private String uri = "https://app.refactory.id/oauth/authorize?client_id=4&response_type=code&redirect_uri=https://app.refactory.id/callback&scope=";
+
+    public void setCode(String newCode){
+        code = newCode;
+    }
+    public String getCode(){
+        return code;
+    }
+
+    public String getURL(){
+        return URL;
+    }
+
+    public String getGrantType(){
+        return grant_type;
+    }
+
+    public String getClient_id(){
+        return client_id;
+    }
+
+    public String getClientSecret(){
+        return client_secret;
+    }
+
+    public String redirectUri(){
+        return redirect_uri;
+    }
+
+    public String getUri(){
+        return uri;
+    }
+
+
 }
