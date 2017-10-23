30351972381eb695708a851d43b9328c9816d66c
pranasabda <pranasabda@gmail.com>
2017-10-23 16:29:19 +0700

    Merge File RegAPI.java from Sam

--- app/src/main/java/id/refactory/app/refactoryapps/api/services/RegAPI.java
+++ app/src/main/java/id/refactory/app/refactoryapps/api/services/RegAPI.java
@@ -1,8 +1,19 @@
 package id.refactory.app.refactoryapps.api.services;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
 /**
  * Created by prana on 18/10/17.
  */
 
 public interface RegAPI {
+    @Headers({
+            "Content-Type: application/json"
+    })
+    @POST("/oauth/token")
+    Call<AuthRequest> setCode(@Body AuthRequest auth);
+
 }
