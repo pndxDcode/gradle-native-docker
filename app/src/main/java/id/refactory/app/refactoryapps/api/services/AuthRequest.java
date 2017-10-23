ba67600f128a881218d3c208f533abfd510f1457
pranasabda <pranasabda@gmail.com>
2017-10-23 16:27:38 +0700

    Merge FIle AuthRequest.java From Sam

--- app/src/main/java/id/refactory/app/refactoryapps/api/services/AuthRequest.java
+++ app/src/main/java/id/refactory/app/refactoryapps/api/services/AuthRequest.java
@@ -1,8 +1,119 @@
 package id.refactory.app.refactoryapps.api.services;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by prana on 18/10/17.
  */
 
 public class AuthRequest {
+    @SerializedName("code")
+    private String code;
+
+    @SerializedName("grant_type")
+    private String grantType;
+
+    @SerializedName("client_id")
+    private String clientId;
+
+    @SerializedName("client_secret")
+    private String clientSecret;
+
+    @SerializedName("redirect_uri")
+    private String redirectUri;
+
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    @SerializedName("expires_in")
+    private Integer expiresIn;
+
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("refresh_token")
+    private String refreshToken;
+
+    public AuthRequest(String code, String grantType, String clientId, String clientSecret, String redirectUri) {
+        this.code = code;
+        this.grantType = grantType;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getGrantType() {
+        return grantType;
+    }
+
+    public void setGrantType(String grantType) {
+        this.grantType = grantType;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+
 }
