55886d80883ef83e8415b9745c784ed9705f645c
MayaRupa <reza@mayarupa.com>
2017-11-01 23:09:55 +0700

    moving package to java>id.refactory...>fragments

--- app/src/main/java/id/refactory/app/refactoryapps/fragments/WPMFragment.java
+++ app/src/main/java/id/refactory/app/refactoryapps/fragments/WPMFragment.java
@@ -17,7 +17,7 @@ import id.refactory.app.refactoryapps.api.models.Datum;
 import id.refactory.app.refactoryapps.api.models.RappMod;
 import id.refactory.app.refactoryapps.api.services.RappClient;
 import id.refactory.app.refactoryapps.api.services.RetrofitConnect;
-import id.refactory.app.refactoryapps.theAdapter.WPMAdapter;
+import id.refactory.app.refactoryapps.adapter.dashboard.WPMAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
