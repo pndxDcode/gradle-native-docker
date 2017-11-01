fae2dc3ef160b9492395c759f6fdbba4f6f976b6
MayaRupa <reza@mayarupa.com>
2017-11-01 22:56:52 +0700

    moving the package to addapter>dashboard

--- app/src/main/java/id/refactory/app/refactoryapps/adapter/dashboard/HRAdapter.java
+++ app/src/main/java/id/refactory/app/refactoryapps/adapter/dashboard/HRAdapter.java
@@ -1,4 +1,4 @@
-package id.refactory.app.refactoryapps.adapter;
+package id.refactory.app.refactoryapps.adapter.dashboard;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
